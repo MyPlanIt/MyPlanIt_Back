c1604ebacc38d37482fd35c20dbe4dbcff39444e
ssssujini99 <sssssjin99@naver.com>
2022-01-01 00:59:59 +0900

    chore: nickname->username 변경

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,29 +3,32 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.settings import api_settings
 
-from django.contrib.auth import authenticate
-from django.contrib.auth.backends import ModelBackend
-
 from .models import User
 
 
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'realname', 'phone_num', 'nickname']
+        fields = ['email', 'password', 'realname', 'phone_num', 'username']
 
     def create(self, validated_data):
         email = validated_data.get('email')
         password = validated_data.get('password')
         realname = validated_data.get('realname')
         phone_num = validated_data.get('phone_num')
-        nickname = validated_data.get('nickname')
+        username = validated_data.get('username')
         user = User(
             email = email,
             realname = realname,
             phone_num = phone_num,
-            nickname = nickname
+            username = username
         )
         user.set_password(password)
         user.save()
         return user
+
+
+class UserSeriallizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'realname', 'username']

ac357fd3796a3c54fc8277fc840ea72cadccea34
ssssujini99 <sssssjin99@naver.com>
2022-01-04 01:14:54 +0900

    feat(accounts/urls.py): 온보딩 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup', views.SignupView.as_view()),
     path('login', views.LoginView.as_view()),
+    path('signup/onbording', views.OnboardingView.as_view()),
 ]

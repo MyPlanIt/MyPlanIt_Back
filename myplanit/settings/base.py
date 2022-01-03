a4f8116aea6860d9007090ac3d277c21279f5cbb
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2022-01-04 04:13:37 +0900

    style(): add domain into ALLOWED_HOSTS

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -30,9 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    'myplanit.cf']
+ALLOWED_HOSTS = ['.myplanit.cf']
 
 
 # Application definition

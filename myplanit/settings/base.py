4c821b1a82ddb0ef1bacbfd60f243b9a03f8d7cb
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2022-01-04 13:37:54 +0900

    init

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -30,9 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [
-    '.ap-northeast-2.compute.amazonaws.com'
-    ,'.myplanit.link']
+ALLOWED_HOSTS = []
 
 
 # Application definition

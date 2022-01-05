3c357c438a352da92bc4e85022244ca2be1a3837
ssssujini99 <sssssjin99@naver.com>
2022-01-05 16:04:02 +0900

    feat(settings/base.py): simple-jwt 세팅 수정

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -137,12 +137,42 @@ REST_FRAMEWORK = {
 }
 
 # JWT 세팅
-SIMPLE_JWT = {
-    # access token 유효기간 하루로 설정
-    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(days=1),
-    # refresh token 유효기간 7일로 설정
-    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7),
-    # Authorization 헤더에 JWT ~~ 형식으로 입력
-    "AUTH_HEADER_TYPES": ("JWT",),
-}
+#SIMPLE_JWT = {
+#    # access token 유효기간 하루로 설정
+#    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(seconds=2),
+#    # refresh token 유효기간 7일로 설정
+#    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7),
+#    # Authorization 헤더에 JWT ~~ 형식으로 입력
+#    "AUTH_HEADER_TYPES": ("JWT",),
+#}
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(seconds=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=100),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': env('DJANGO_SECRET_KEY'),
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
+}

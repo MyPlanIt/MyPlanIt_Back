8eb4499191ad49c4662b44d6ceac47cfe98139bf
ssssujini99 <sssssjin99@naver.com>
2022-01-06 22:39:26 +0900

    feat(settings/base.py): plan앱 추가, taggit패키지 추가

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'accounts',
     'multiselectfield',
     'rest_framework_simplejwt',
+    'taggit',
+    'accounts',
+    'plan',
 ]
 
 

818f38c549da4f5ba838015081eda2e0f946b584
ssssujini99 <sssssjin99@naver.com>
2022-01-07 22:02:28 +0900

    chore(settings/base.py): Debug=True로 세팅

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

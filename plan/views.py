abf205c3f91cb187bfce415acbe4c0d76c213412
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-08 07:17:14 +0900

    feat(plan/views.py): add buying plan logic

--- plan/views.py
+++ plan/views.py
@@ -29,6 +29,28 @@ class PlanDetailView(APIView):
         return Response(serializer.data)
 
 
+    # 특정 플랜 구매
+    def post(self, request, pk):
+
+        try:
+            res = list(jwt_token.get_token(request))
+            user = res[0]  # 토큰으로 유저 조회
+            plan = get_object_or_404(Plan, id=pk)
+
+            if User_Plan.objects.filter(user=user).filter(plan=plan) is not None:
+                return Response({"message": "이미 구매한 플랜입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+            else:
+                user_plan = User_Plan()
+                user_plan.user = user
+                user_plan.plan = plan
+                user_plan.own_flag = True
+                return Response({"message": "구매 완료"}, status=status.HTTP_200_OK)
+
+        except:
+            return Response({"message": "로그인이 만료되었습니다"}, status=status.HTTP_400_BAD_REQUEST)
+
+
 # 유저 소유 플랜 조회
 class OwnPlanView(APIView):
     def get(self, request):

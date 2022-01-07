a9f28ae3579a2bd9bbd9ad95145eb95d84ddda63
ssssujini99 <sssssjin99@naver.com>
2022-01-07 23:12:53 +0900

    chore: 200으로 상태코드 변경

--- accounts/views.py
+++ accounts/views.py
@@ -31,7 +31,7 @@ class SignupView(APIView):
             response.set_cookie('access_token', access_token)
             response.set_cookie('refresh_token', refresh_token)
             return response
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 회원가입 에러인 경우(email, username 검증에서 탈락 시)
+        return Response(serializer.errors, status=status.HTTP_200_OK) # 회원가입 에러인 경우(email, username 검증에서 탈락 시)
 
 
 # 로그인
@@ -87,12 +87,12 @@ class LoginView(APIView):
 
         if user is None: # 해당 email의 user가 존재하지 않는 경우
             return Response(
-                {"message": "존재하지않는 email입니다."}, status=status.HTTP_400_BAD_REQUEST
+                {"message": "존재하지않는 email입니다."}, status=status.HTTP_200_OK
             )
 
         if not check_password(password, user.password): # 비밀번호에서 틀린 경우
             return Response(
-                {"message": "비밀번호가 틀렸습니다."}, status=status.HTTP_400_BAD_REQUEST
+                {"message": "비밀번호가 틀렸습니다."}, status=status.HTTP_200_OK
             )
 
         if user is not None: # 모두 성공 시
@@ -115,7 +115,7 @@ class LoginView(APIView):
             return response
         else: # 그 외
             return Response(
-                {"message": "로그인에 실패하였습니다"}, status=status.HTTP_400_BAD_REQUEST
+                {"message": "로그인에 실패하였습니다"}, status=status.HTTP_200_OK
             )
 
 

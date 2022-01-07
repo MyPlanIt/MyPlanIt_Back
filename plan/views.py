2e637e0b06f2c1265e0ae4c7973f9cea23f694a0
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-08 05:37:30 +0900

    feat(plan.views.py): add OwnPlanView
    유저가 소유하고 있는 플랜을 조회하는 view 작성

--- plan/views.py
+++ plan/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .models import Plan
-from .serializers import PlanSerializer, PlanDetailSerializer
-
-# Create your views here.
+from .models import Plan, User_Plan
+from .serializers import PlanSerializer, PlanDetailSerializer, OwnPlanSerializer, UserPlanSerializer
+from jwt_token import jwt_token
 
 
 # 전체 플랜 조회
@@ -18,7 +17,7 @@ class PlanView(APIView):
         growth_serializer = PlanSerializer(growth, many=True).data
 
         return Response({"Routine": routine_serializer,
-                          "Growth": growth_serializer},
+                         "Growth": growth_serializer},
                         status=status.HTTP_200_OK)
 
 
@@ -28,3 +27,26 @@ class PlanDetailView(APIView):
         plan = get_object_or_404(Plan, id=pk)
         serializer = PlanDetailSerializer(plan)
         return Response(serializer.data)
+
+
+# 유저 소유 플랜 조회
+class OwnPlanView(APIView):
+    def get(self, request):
+
+        try:
+            res = list(jwt_token.get_token(request))
+            user = res[0]  # 토큰으로 유저 조회
+            user_plan = User_Plan.objects.filter(user=user).filter(own_flag=True)  # 토큰으로 조회한 유저의 own plan 저장
+            serializer = UserPlanSerializer(user_plan, many=True).data
+
+            plan_res = list()  # JSON 값 저장할 리스트 생성
+            for i in range(len(serializer)):
+                plan = serializer[i]
+                own_plan = Plan.objects.filter(id=plan['plan'])
+                own_plan_serializer = OwnPlanSerializer(own_plan, many=True).data
+                plan_res.append(own_plan_serializer)
+
+            return Response(plan_res, status=status.HTTP_200_OK)
+
+        except:
+            return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)

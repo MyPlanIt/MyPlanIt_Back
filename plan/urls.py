6cf7497682dad418bc2f4e543adb5ecc0e488bec
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-09 01:59:54 +0900

    chore(plan.urls.py): add url of buying plan
    add /buy to url

--- plan/urls.py
+++ plan/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('plan/', views.PlanView.as_view()),
     path('plan/<int:pk>', views.PlanDetailView.as_view()),
+    path('plan/<int:pk>/buy', views.PlanBuyView.as_view()),
     path('myplan/', views.OwnPlanView.as_view()),
 ]

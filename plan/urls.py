39dc95177e9553aa4874d7199e4be4be41b706af
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-10 03:26:54 +0900

    feat(plan/urls.py): add myplan, myplan/buy url
    myplan : 유저가 찜한 플랜들을 보여주는 url
    myplan/buy : 유저가 구매한 플랜들을 보여주는 url

--- plan/urls.py
+++ plan/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('plan/', views.PlanView.as_view()),
+    path('plan', views.PlanView.as_view()),
     path('plan/<int:pk>', views.PlanDetailView.as_view()),
     path('plan/<int:pk>/buy', views.PlanBuyView.as_view()),
     path('plan/<int:pk>/wish', views.PlanWishView.as_view()),
-    path('myplan/', views.OwnPlanView.as_view()),
+    path('myplan', views.WishPlanView.as_view()),
+    path('myplan/buy', views.OwnPlanView.as_view()),
 ]

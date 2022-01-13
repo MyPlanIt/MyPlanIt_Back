0cd6cc3bdc06ab355958b3e805927d234d7cc23f
ssssujini99 <sssssjin99@naver.com>
2022-01-14 02:48:49 +0900

    feat(todo/urls.py): PlanTodoDelayAPIView url 추가

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('todo/plan/<str:pk>', views.PlanTodoAPIView.as_view()), # 해당 날짜의 플랜 투두 조회
     path('todo/plan/<int:plan_id>/<int:plan_todo_id>', views.PlanTodoCheckAPIView.as_view()), # 플랜 투두 완료 기능
+    path('todo/plan/<int:plan_todo_id>/delay', views.PlanTodoDelayAPIView.as_view()), # 플랜 투두 미루기 기능
 ]

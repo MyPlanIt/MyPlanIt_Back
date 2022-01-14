fdbc11021500643d56acf38a812fbdccae429165
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-15 05:15:45 +0900

    feat(todo/model): add User_personal_todo model
    add model and migrations

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from accounts.models import User
+
+
+class User_personal_todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo_name = models.CharField(max_length=100)
+    date = models.DateField()
+    finish_flag = models.BooleanField(default=False)
+    delete_flag = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todo_name

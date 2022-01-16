420173e1d21a2f738799238a7f86f35ae6a1e688
Sukyeong Seo <tjtnrud0967@gmail.com>
2022-01-17 01:44:05 +0900

    refactor(todo): reduce to one view
    수정, 삭제, 이름 변경(수정) 기능을 하나의 view로 수정 (method만 다르게)

--- todo/urls.py
+++ todo/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todos/plan/<str:pk>', views.PlanTodoAPIView.as_view()), # 해당 날짜의 플랜 투두 조회
-    path('todos/plan/<int:plan_id>/<int:plan_todo_id>', views.PlanTodoCheckAPIView.as_view()),  # 플랜 투두 완료 기능
-    path('todos/plan/<int:plan_todo_id>/delay', views.PlanTodoDelayAPIView.as_view()),  # 플랜 투두 미루기 기능
-    path('todos/plan/<int:plan_id>/todos', views.AllTodoAPIView.as_view()),  # 플랜 클릭 시 전체 투두 조회 기능
-    path('todos/plan/<int:plan_todo_id>/detail', views.DetailTodoAPIView.as_view()),  # 투두 세부 정보 조회 기능
+    path('todos/plan/<str:date>', views.PlanTodoView.as_view()),  # 해당 날짜의 플랜 투두 조회
+    path('todos/plan/<int:plan_id>/<int:plan_todo_id>/check', views.CheckPlanTodoView.as_view()),  # 플랜 투두 완료 기능
+    path('todos/plan/<int:plan_todo_id>/delay', views.DelayPlanTodoView.as_view()),  # 플랜 투두 미루기 기능
+    path('todos/plan/<int:plan_id>/todos', views.PlanTodosView.as_view()),  # 플랜 클릭 시 전체 투두 조회 기능
+    path('todos/plan/<int:plan_todo_id>/detail', views.DetailTodoView.as_view()),  # 투두 세부 정보 조회 기능
 
-    path('todos/my/<str:date>', views.PersonalTodoAPIVIew.as_view()),  # 개인 투두 조회, 추가
-    path('todos/my/<int:id>/check', views.PersonalTodoCheckAPIView.as_view()),  # 개인 투두 완료 기능
-    path('todos/my/<int:id>/delete', views.PersonalTodoDeleteAPIView.as_view()),  # 개인 투두 삭제 기능
-    path('todos/my/<int:id>/edit', views.PersonalTodoEditAPIView.as_view()),  # 개인 투두 수정 기능
-    path('todos/my/<int:id>/delay', views.PersonalTodoDelayAPIView.as_view())  # 개인 투두 미루기 기능
+    path('todos/my/<str:date>', views.MyTodoVIew.as_view()),  # 개인 투두 조회, 추가
+    path('todos/my/<int:id>/check', views.EditMyTodoView.as_view()),  # 개인 투두 완료 기능
+    path('todos/my/<int:id>/delete', views.EditMyTodoView.as_view()),  # 개인 투두 삭제 기능
+    path('todos/my/<int:id>/edit', views.EditMyTodoView.as_view()),  # 개인 투두 이름 변경(수정) 기능
+    path('todos/my/<int:id>/delay', views.DelayMyTodoView.as_view())  # 개인 투두 미루기 기능
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -18,8 +17,8 @@ def get_user(request):
 
 
 # 해당 날짜의 플랜 투두 조회
-class PlanTodoAPIView(APIView):
-    def get(self, request, pk):  # pk의 default = 현재 날짜 값 (ex, 20-01-11)
+class PlanTodoView(APIView):
+    def get(self, request, date):  # pk의 default = 현재 날짜 값 (ex, 20-01-11)
         User_plan_todo.objects.all().order_by('plan_id', 'plan_todo_id')  # plan의 id값으로 1차 정렬 -> plan_todo_id로 2차 정렬
         try:
             plan_querysets = User_Plan.objects.filter(user=get_user(request))  # User_Plan에서 user가 갖고 있는 plans들 가져오기
@@ -28,7 +27,7 @@ class PlanTodoAPIView(APIView):
 
             for plan_queryset in plan_querysets:  # 하나의 플랜에서 플랜 투두들 가져오기 위함
                 plan = plan_queryset.plan  # 플랜 가져오기
-                plan_todo_querysets = User_plan_todo.objects.filter(user=get_user(request), date=pk, plan=plan)  # user_plan_todo모델에서 해당 유저, 해당 날짜, 해당 플랜의 플랜투두들 가져오기
+                plan_todo_querysets = User_plan_todo.objects.filter(user=get_user(request), date=date, plan=plan)  # user_plan_todo모델에서 해당 유저, 해당 날짜, 해당 플랜의 플랜투두들 가져오기
                 serializer = UserPlanTodoSerializer(plan_todo_querysets, many=True)  # serializer를 이용해 plan todo들을 가공된 Json data로 변환
                 rate = plan_queryset.rate  # 플랜의 달성률 가져오기
                 serializer_data = list(serializer.data)
@@ -42,37 +41,35 @@ class PlanTodoAPIView(APIView):
 
 
 # 플랜 투두 완료 기능 (체크 기능)
-class PlanTodoCheckAPIView(APIView):
-   def post(self, request, plan_id, plan_todo_id):
-       try:
-           res = list(jwt_token.get_token(request))
-           user = res[0] # 토큰으로 유저 조회
-           user_plan_todo = get_object_or_404(User_plan_todo, id=plan_todo_id)
+class CheckPlanTodoView(APIView):
+    def post(self, request, plan_id, plan_todo_id):
+        try:
+            user_plan_todo = get_object_or_404(User_plan_todo, id=plan_todo_id)
 
-           if user_plan_todo.finish_flag == 0: # 미완료 -> 완료인 경우
-               user_plan_todo.finish_flag = 1
-           else: # user_plan_todo.finish_flag == 1 # 완료 -> 미완료 경우
-               user_plan_todo.finish_flag = 0
+            if user_plan_todo.finish_flag == 0:  # 미완료 -> 완료인 경우
+                user_plan_todo.finish_flag = 1
+            else:  # user_plan_todo.finish_flag == 1 # 완료 -> 미완료 경우
+                user_plan_todo.finish_flag = 0
 
-           user_plan_todo.save()
+            user_plan_todo.save()
 
-           # 달성률 업데이트해주기
-           user_plan = User_Plan.objects.filter(user=user, plan_id=plan_id)[0] # 해당 플랜
-           user_plan_todos = User_plan_todo.objects.filter(user=user, plan_id=plan_id) # 해당 플랜의 투두들
-           res = 0 # 누적합
-           i = 0 # 투두 개수
-           for user_plan_todo in user_plan_todos:
-               res += user_plan_todo.finish_flag
-               i += 1
-           user_plan.rate = (res / i) * 100 # rate: 0 ~ 100 (% 단위로 표현)
-           user_plan.save()
-           return Response({"message": "success"}, status=status.HTTP_200_OK)
-       except:
-           return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
+            # 달성률 업데이트해주기
+            user_plan = User_Plan.objects.filter(user=get_user(request), plan_id=plan_id)[0]  # 해당 플랜
+            user_plan_todos = User_plan_todo.objects.filter(user=get_user(request), plan_id=plan_id)  # 해당 플랜의 투두들
+            res = 0  # 누적합
+            i = 0  # 투두 개수
+            for user_plan_todo in user_plan_todos:
+                res += user_plan_todo.finish_flag
+                i += 1
+            user_plan.rate = (res / i) * 100  # rate: 0 ~ 100 (% 단위로 표현)
+            user_plan.save()
+            return Response({"message": "success"}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 플랜 투두 내일하기 기능
-class PlanTodoDelayAPIView(APIView):
+class DelayPlanTodoView(APIView):
     def post(self, request, plan_todo_id):
         try:
             user_plan_todo = get_object_or_404(User_plan_todo, id=plan_todo_id)
@@ -84,7 +81,7 @@ class PlanTodoDelayAPIView(APIView):
 
 
 # 플랜 클릭 시 전체 투두 조회 기능
-class AllTodoAPIView(APIView):
+class PlanTodosView(APIView):
     def get(self, request, plan_id):
         try:
             plan = get_object_or_404(Plan, id=plan_id)
@@ -98,11 +95,11 @@ class AllTodoAPIView(APIView):
 
 
 # 투두 하나 클릭 시 세부 정보 조회
-class DetailTodoAPIView(APIView):
+class DetailTodoView(APIView):
     def get(self, request, plan_todo_id):
         try:
             plan_todo = get_object_or_404(Plan_todo, id=plan_todo_id)
-            if plan_todo.media_flag == False:  # 이미지만 있는 경우
+            if not plan_todo.media_flag:  # 이미지만 있는 경우
                 return Response({"image_url": plan_todo.img_url}, status=status.HTTP_200_OK)
             else:
                 media_querysets = Plan_todo_video.objects.filter(plan_todo_id=plan_todo_id)
@@ -113,7 +110,7 @@ class DetailTodoAPIView(APIView):
 
 
 # 개인 투두 조회, 추가
-class PersonalTodoAPIVIew(APIView):
+class MyTodoVIew(APIView):
     def get(self, request, date):
         try:
             user_todos = User_personal_todo.objects.filter(user=get_user(request)).filter(date=date).order_by('-id')
@@ -137,8 +134,9 @@ class PersonalTodoAPIVIew(APIView):
             return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# 개인 투두 완료 기능
-class PersonalTodoCheckAPIView(APIView):
+# 개인 투두 수정 기능
+class EditMyTodoView(APIView):
+    # 개인 투두 완료 체크
     def post(self, request, id):
         try:
             user_todo = get_object_or_404(User_personal_todo, user=get_user(request), id=id)
@@ -156,22 +154,8 @@ class PersonalTodoCheckAPIView(APIView):
         except:
             return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-
-# 개인 투두 삭제 기능
-class PersonalTodoDeleteAPIView(APIView):
-    def post(self, request, id):
-        try:
-            user_todo = get_object_or_404(User_personal_todo, user=get_user(request), id=id)
-            user_todo.delete()
-            return Response({"message": "투두를 삭제하였습니다."}, status=status.HTTP_200_OK)
-
-        except:
-            return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
-
-
-# 개인 투두 수정 기능
-class PersonalTodoEditAPIView(APIView):
-    def post(self, request, id):
+    # 개인 투두 이름 변경
+    def patch(self, request, id):
         try:
             user_todo = get_object_or_404(User_personal_todo, user=get_user(request), id=id)
 
@@ -182,9 +166,19 @@ class PersonalTodoEditAPIView(APIView):
         except:
             return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
+    # 개인 투두 삭제
+    def delete(self, request, id):
+        try:
+            user_todo = get_object_or_404(User_personal_todo, user=get_user(request), id=id)
+            user_todo.delete()
+            return Response({"message": "투두를 삭제하였습니다."}, status=status.HTTP_200_OK)
+
+        except:
+            return Response({"message": "로그인이 만료되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # 개인 투두 내일하기 기능
-class PersonalTodoDelayAPIView(APIView):
+class DelayMyTodoView(APIView):
     def post(self, request, id):
         try:
             user_todo = get_object_or_404(User_personal_todo, user=get_user(request), id=id)

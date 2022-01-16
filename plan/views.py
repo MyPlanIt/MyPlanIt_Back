f041ce2f7e3cc4a7bcff8092819ff4f0d8b9706c
Sukyeong Seo <ssk0967@naver.com>
2022-01-17 02:34:42 +0900

    Update views.py

--- plan/views.py
+++ plan/views.py
@@ -18,7 +18,7 @@ def get_user_and_plan(request, pk):
 
 # 전체 플랜 조회
 class PlanView(APIView):
-    def get(self):
+    def get(self, request):
         routine = Plan.objects.filter(category="Routine")
         growth = Plan.objects.filter(category="Growth")
 
@@ -32,7 +32,7 @@ class PlanView(APIView):
 
 # 특정 플랜 조회
 class PlanDetailView(APIView):
-    def get(self, pk, format=None):
+    def get(self, request, pk, format=None):
         plan = get_object_or_404(Plan, id=pk)
         serializer = PlanDetailSerializer(plan)
         return Response(serializer.data)

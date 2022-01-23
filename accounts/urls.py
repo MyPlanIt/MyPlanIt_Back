37441940410ae2bd99efa7270d8ee10ae92b1f2f
ssssujini99 <sssssjin99@naver.com>
2022-01-24 03:46:29 +0900

    chore(accounts/urls.py): 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.SignupView.as_view()),
-    path('login', views.LoginView.as_view()),
-    path('signup/onboarding', views.OnboardingView.as_view()),
+    path('signup', views.SignupView.as_view()),  # 회원 가입
+    path('login', views.LoginView.as_view()),  # 로그인
+    path('signup/onboarding', views.OnboardingView.as_view()),  # 온보딩
 ]

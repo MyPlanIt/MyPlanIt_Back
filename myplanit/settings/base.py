40a54c9ef0ead848c4dea2c9513767fd1026ee10
ssssujini99 <sssssjin99@naver.com>
2022-01-24 03:48:55 +0900

    chore(settings/base.py): drf-jwt로 토큰인증방식 변경

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'rest_framework',
     'corsheaders',
     'multiselectfield',
-    'rest_framework_simplejwt',
     'taggit',
     'django_filters',
     'accounts',
@@ -68,6 +69,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myplanit.urls'
+
 
 TEMPLATES = [
     {
@@ -159,40 +161,26 @@ CORS_ALLOWED_ORIGINS = (
     "https://my-plan-it-front.vercel.app"
 )
 
-# simple_JWT 추가
+
+# JWT permission, authentication 세팅
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
     'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
+
 # JWT 세팅
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=30),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
-    'UPDATE_LAST_LOGIN': False,
-
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': env('DJANGO_SECRET_KEY'),
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-
-    'JTI_CLAIM': 'jti',
+JWT_AUTH = {
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',  # 암호화 알고리즘
+    'JWT_ALLOW_REFRESH': True, # jwt 갱신 여부
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=15), # jwt 토큰 유효기간
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=30), # jwt 토큰 갱신 유효기간
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }

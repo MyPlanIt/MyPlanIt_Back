44ec58427cd4e47bc6fec1f870b8cc208d480089
ssssujini99 <sssssjin99@naver.com>
2022-02-26 00:38:02 +0900

    edit

--- accounts/views.py
+++ accounts/views.py
@@ -55,7 +55,7 @@ def kakao_login(request):
 # 카카오 회원가입 & 로그인
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def kakao_callback(self, request, format=None):
+def kakao_callback(request, format=None):
     app_rest_api_key = "41a2c19cd51500b22e399c7019defd4c"
     # redirect_uri = "http://127.0.0.1:8000/login/kakao/callback"
     redirect_uri = "https://myplanit.link/login/kakao/callback"

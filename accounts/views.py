e996edbc4c9964bed2bc2726bd1977eda8627811
ssssujini99 <sssssjin99@naver.com>
2022-02-26 00:41:32 +0900

    edit

--- accounts/views.py
+++ accounts/views.py
@@ -60,9 +60,9 @@ def kakao_callback(request, format=None):
     # redirect_uri = "http://127.0.0.1:8000/login/kakao/callback"
     redirect_uri = "https://myplanit.link/login/kakao/callback"
     # client_secret = env('SECRET')
-    # code = request.GET.get('code')
-    request_body = json.loads(request.body)
-    code = request_body.get('code', None)
+    code = request.GET.get('code')
+    # request_body = json.loads(request.body)
+    # code = request_body.get('code', None)
     # code = request.data["code"]
     headers = {
         'Access-Control-Allow-Origin': 'https://www.myplanit.site',

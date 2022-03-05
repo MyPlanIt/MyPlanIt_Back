4f751d14b19147402a17dc2d3dfd2fca87d17dde
ssssujini99 <sssssjin99@naver.com>
2022-03-05 11:54:37 +0900

    change settings.py debug

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 

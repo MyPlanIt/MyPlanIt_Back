5b969fc98080c0f4c08c80bbef591d90966322db
ssssujini99 <sssssjin99@naver.com>
2022-03-10 17:03:49 +0900

    feat: landingpage form model 작성

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class landingPage(models.Model):
+    phoneNum = models.CharField(max_length=20)  # 전화번호(필수)
+    job = models.CharField(max_length=20)  # 직업(필수)
+    todoPlan = models.TextField(blank=True)  # 원하는 TO-DO 플랜(선택)
+    created_at = models.DateTimeField(auto_now_add=True)  # 작성시간(자동)

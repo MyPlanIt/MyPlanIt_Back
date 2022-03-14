8338027bfa8d8f1c2be00b7d8b127ce0d552fd46
sukyeong <ssk0967@naver.com>
2022-03-15 05:11:54 +0900

    chore(accounts/models.py): extend max_length of username
    구글전용 식별 id 길이가 많이 길어서 20→50으로 수정

--- accounts/models.py
+++ accounts/models.py
@@ -40,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     username = models.CharField(
-        max_length=20,
+        max_length=50,
         unique=True
     )
     realname = models.CharField(

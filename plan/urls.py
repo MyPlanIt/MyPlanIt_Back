3f8935d772e513bb14597a3cb97348321e25cc95
ssssujini99 <sssssjin99@naver.com>
2022-06-25 20:27:23 +0900

    Chore(plan/): add TodoChallengeView

--- plan/urls.py
+++ plan/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('myplans/wish', views.WishPlansView.as_view()),  # 유저가 찜한 플랜 조회
     path('myplans/buy', views.BuyPlansView.as_view()),  # 유저가 구매한 플랜 조회
     path('myplans/registered', views.RegisteredPlanView.as_view()),  # 유저가 등록한 플랜 조회
+    path('myplans/<int:pk>/challenge', views.TodoChallengeView.as_view()), # 구매 & 등록
     path('myplans/<int:pk>/register', views.RegisterPlanView.as_view()),  # 구매한 플랜 등록
     path('myplans/<int:pk>/delete', views.DeletePlanView.as_view()),  # 등록한 플랜 삭제
 
--- plan/views.py
+++ plan/views.py
@@ -69,6 +69,53 @@ class BuyPlanView(APIView):
             return Response({"message": "로그인이 만료되었습니다"}, status=status.HTTP_400_BAD_REQUEST)
 
 
+
+# 플랜 구매 + 등록
+class TodoChallengeView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request, pk):
+        try:
+            plan = get_object_or_404(Plan, id=pk)
+            user_own_plan = User_Plan.objects.filter(user=request.user).filter(plan=plan).filter(own_flag=True)
+            user_plan = User_Plan.objects.filter(user=request.user).filter(plan=plan)
+
+            if user_own_plan.exists():
+                return Response({"message": "이미 구매한 플랜입니다."}, status=status.HTTP_208_ALREADY_REPORTED)
+
+            elif user_plan.exists():
+                user_plan.update(own_flag=True)
+                return Response({"message": "구매 완료"}, status=status.HTTP_200_OK)
+
+            else:
+                user_plan = User_Plan.objects.create(user=request.user, plan=plan, own_flag=True)
+                user_plan.save()
+
+                plan_todos = Plan_todo.objects.filter(plan=plan)
+
+                for plan_todo in plan_todos:
+                    date = timezone.now()  # utc 변경한 부분
+                    date += datetime.timedelta(days=plan_todo.date)  # 날짜 + 걸리는 일수에 맞게 db에 넣어주기
+                    user_plan_todo = User_plan_todo(user=request.user, plan=plan, plan_todo=plan_todo, date=date,
+                                                    day=plan_todo.date)  # day field 추가
+                    user_plan_todo.save()
+
+                user_plan.register_flag = True  # 등록 flag = True 로 변경
+
+                # User_Plan 모델에 start_date, finish_date 추가
+                user_plan = User_Plan.objects.get(user=request.user, plan=plan)
+
+                user_plan.start_date = User_plan_todo.objects.filter(user=request.user, plan=plan).first().date
+                user_plan.finish_date = User_plan_todo.objects.filter(user=request.user, plan=plan).last().date
+                user_plan.save()
+
+                return Response({"message": "등록 완료"}, status=status.HTTP_200_OK)
+
+        except:
+            return Response({"message": "로그인이 만료되었습니다"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 # 특정 플랜 찜하기
 class WishPlanView(APIView):
     permission_classes = (IsAuthenticated,)
